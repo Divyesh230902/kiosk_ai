48a2da08bf527d7881556b4407dad46c335ca496
Divyesh Patel <86349765+Divyesh230902@users.noreply.github.com>
2024-07-14 11:55:30 +0530

    gTTS converted

--- tts_engine.py
+++ tts_engine.py
@@ -1,20 +1,20 @@
-# import pyttsx3
-# import os
-# from datetime import datetime
-# from gtts import gTTS
+import pyttsx3
+import os
+from datetime import datetime
+from gtts import gTTS
 
-# def speak(text):
+def speak(text):
 
-#     output_dir = 'output/'
-#     # Generate the file name with the current date and time
-#     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-#     file_name = f"{output_dir}response_{timestamp}.mp3"
-#     # file_path = os.path.join(output_dir, file_name)
-#     file_path = os.path.abspath(file_name)
+    output_dir = 'output/'
+    # Generate the file name with the current date and time
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    file_name = f"{output_dir}response_{timestamp}.mp3"
+    # file_path = os.path.join(output_dir, file_name)
+    file_path = os.path.abspath(file_name)
 
-#     tts = gTTS(text,lang="en")
-#     tts.save(file_path)
-#     return file_name
+    tts = gTTS(text,lang="en")
+    tts.save(file_path)
+    return file_name
 
 # def speak(text):
 #     output_dir = 'output/'
@@ -35,33 +35,33 @@
 #     return file_path
 
 
-from TTS.api import TTS
-from datetime import datetime
-import os
-from scipy.io import wavfile
-import noisereduce as nr
+# from TTS.api import TTS
+# from datetime import datetime
+# import os
+# from scipy.io import wavfile
+# import noisereduce as nr
 
 
-def speak(text):
-    # Initialize TTS model
-    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
-    output_dir = 'output/'
-#    Generate the file name with the current date and time
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = f"{output_dir}response_{timestamp}.mp3"
-    # file_path = os.path.join(output_dir, file_name)
-    file_path = os.path.abspath(file_name)
+# def speak(text):
+#     # Initialize TTS model
+#     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=False)
+#     output_dir = 'output/'
+# #    Generate the file name with the current date and time
+#     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+#     file_name = f"{output_dir}response_{timestamp}.mp3"
+#     # file_path = os.path.join(output_dir, file_name)
+#     file_path = os.path.abspath(file_name)
 
-    # Generate speech to file
-    output_path = "output.wav"
-    rate, data = wavfile.read(output_path)
+#     # Generate speech to file
+#     output_path = "output.wav"
+#     rate, data = wavfile.read(output_path)
 
-    # Perform noise reduction
-    reduced_noise = nr.reduce_noise(y=data, sr=rate)
+#     # Perform noise reduction
+#     reduced_noise = nr.reduce_noise(y=data, sr=rate)
 
-    # Write noise-reduced audio to file
-    output_reduced_noise_path = "output.wav"
-    wavfile.write(output_reduced_noise_path, rate, reduced_noise)
-    tts.tts_to_file(text=text, file_path=file_path,
-                    speaker_wav=output_reduced_noise_path, language="en", split_sentences=True)
-    return file_path
+#     # Write noise-reduced audio to file
+#     output_reduced_noise_path = "output.wav"
+#     wavfile.write(output_reduced_noise_path, rate, reduced_noise)
+#     tts.tts_to_file(text=text, file_path=file_path,
+#                     speaker_wav=output_reduced_noise_path, language="en", split_sentences=True)
+#     return file_path
